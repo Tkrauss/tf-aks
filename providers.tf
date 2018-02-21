a2f9ae47ffc019411b120c82cf461d8fad50c396
Darius Tehrani <dt@dariustehrani.io>
2018-02-21 22:44:26 +0100

    Initial structure

---
+++ providers.tf
@@ -0,0 +1,17 @@
+# Please do NOT set these credentials here.
+provider "azurerm" {
+}
+
+/*
+Please maintain the credentials outside your git repository e.g. through environment variables.
+ARM_SUBSCRIPTION_ID
+ARM_TENANT_ID
+*/
+
+
+# Resource Group
+resource "azurerm_resource_group" "development" {
+  name     = "${var.projectname}-dev"
+  location = "West Europe"
+}
+
