ea8d2f979178449f629ad377c87a0474033b3e92
Darius Tehrani <dt@dariustehrani.io>
2018-02-24 21:54:26 +0100

    remote state bootstrap.w

---
+++ bootstrap_remote_state/variables.tf
@@ -0,0 +1,30 @@
+variable "projectname"  {
+description = "set the name of the project"
+type = "string"
+}
+
+variable "location"  {
+description = "set Azure Location e.g. eastus or westeurope"
+type = "string"
+}
+
+variable "tf_aks_state_sa_replication_type"  {
+description = "set replication type for the Storage Account"
+type = "string"
+default = "LRS"
+}
+
+variable "tf_aks_state_sa_tier"  {
+description = "Set the Storage Tier e.g. to Standard or Premium"
+type = "string"
+default = "Standard"
+}
+
+
+
+variable "resource_tags" {
+type = "map"
+default = {
+    Name = "aks_state"
+    }
+}
