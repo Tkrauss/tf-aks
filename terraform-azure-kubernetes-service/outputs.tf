cf77b44b2fe6da529736e74398427b629d2dc843
Darius Tehrani <dt@dariustehrani.io>
2018-02-25 21:53:26 +0100

    extended outputs.

--- terraform-azure-kubernetes-service/outputs.tf
+++ terraform-azure-kubernetes-service/outputs.tf
@@ -1,3 +1,19 @@
 output "resource-group-name" {
   value = "${azurerm_resource_group.tf_aks.name}"
 }
+
+output "aks-cluster-id" {
+  value = "${azurerm_kubernetes_cluster.tf_aks.id}"
+}
+
+output "aks-cluster-fqdn" {
+  value = "${azurerm_kubernetes_cluster.tf_aks.fqdn}"
+}
+
+output "aks-cluster-name" {
+  value = "${azurerm_kubernetes_cluster.tf_aks.name}"
+}
+
+output "aks-cluster-location" {
+  value = "${azurerm_kubernetes_cluster.tf_aks.location}"
+}
