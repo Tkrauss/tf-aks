18157a9c40f05d9eca7120ec99462eef10ac6b15
Darius Tehrani <dt@dariustehrani.io>
2018-02-26 13:02:30 +0100

    commented out output of fqdn

--- terraform-azure-kubernetes-service/outputs.tf
+++ terraform-azure-kubernetes-service/outputs.tf
@@ -6,14 +6,15 @@ output "aks-cluster-id" {
   value = "${azurerm_kubernetes_cluster.tf_aks.id}"
 }
 
+/*
 output "aks-cluster-fqdn" {
   value = "${azurerm_kubernetes_cluster.tf_aks.fqdn}"
 }
-/*
+*/
+
 output "aks-cluster-name" {
   value = "${azurerm_kubernetes_cluster.tf_aks.name}"
 }
-*/
 
 output "aks-cluster-location" {
   value = "${azurerm_kubernetes_cluster.tf_aks.location}"
