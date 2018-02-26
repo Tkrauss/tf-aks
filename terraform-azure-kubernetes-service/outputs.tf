73969ca65ffb9184edb285e7d71aadedf19119d3
Darius Tehrani <dt@dariustehrani.io>
2018-02-26 09:59:00 +0100

    removed fqdn output

--- terraform-azure-kubernetes-service/outputs.tf
+++ terraform-azure-kubernetes-service/outputs.tf
@@ -9,10 +9,11 @@ output "aks-cluster-id" {
 output "aks-cluster-fqdn" {
   value = "${azurerm_kubernetes_cluster.tf_aks.fqdn}"
 }
-
+/*
 output "aks-cluster-name" {
   value = "${azurerm_kubernetes_cluster.tf_aks.name}"
 }
+*/
 
 output "aks-cluster-location" {
   value = "${azurerm_kubernetes_cluster.tf_aks.location}"
