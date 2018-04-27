04ac0e92df94c5274cde619fcc902d8fa9aea267
Tilman Krauss <tkrauss@tecracer.de>
2018-04-27 09:19:00 +0200

    enabled existing ressource group reusement. Removed tagging to rely on ressource-group mechanism for simplicity

--- bootstrap-azure-remote-state/bootstrap_remote_state.tf
+++ bootstrap-azure-remote-state/bootstrap_remote_state.tf
@@ -6,13 +6,22 @@
 
 provider "azurerm" {}
 
+locals {
+  ressource_group_name   = "${var.use_ressource_group == "-" ? concat(var.projectname, "-", var.location, "-tfstate") : var.use_ressource_group}"
+  create_ressource_group = "${var.use_ressource_group == "-"}"
+}
+
 # Resource Group
 # Required for the Storage Resources
+# Is created if var.use_ressource_group == "-"
 resource "azurerm_resource_group" "tf_aks_state" {
-  name     = "${var.projectname}-${var.location}-tfstate"
+  count    = "${locals.create_ressource_group}"
+  name     = "${locals.ressource_group_name}"
   location = "${var.location}"
 
-  # tags = "${(var.resource_tags, map("Name", "${var.projectname}-${var.location}-tfstate"))}"
+  lifecycle {
+    prevent_destroy = "${var.prevent_destroy}"
+  }
 }
 
 # Generates a Random ID to be used for the SA name.
@@ -28,30 +37,26 @@ resource "random_id" "tf_aks_state_sa_random" {
 resource "azurerm_storage_account" "tf_aks_state_sa" {
   name                      = "${random_id.tf_aks_state_sa_random.hex}"
   location                  = "${var.location}"
-  resource_group_name       = "${azurerm_resource_group.tf_aks_state.name}"
+  resource_group_name       = "${locals.ressource_group_name}"
   account_replication_type  = "${var.tf_aks_state_sa_replication_type}"
-  account_tier              = "${var.tf_aks_state_sa_tier}"
+  account_tier              = "Standard"
   enable_file_encryption    = "true"
   enable_https_traffic_only = "true"
   account_encryption_source = "Microsoft.Storage"
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = "${var.prevent_destroy}"
   }
-
-  #  tags = "${(var.resource_tags, map("Name", "${var.projectname}-${var.location}-tfstate"))}"
 }
 
 # Creation of the storage Container
 resource "azurerm_storage_container" "tf_aks_state" {
   name                  = "${var.projectname}-tf-state"
-  resource_group_name   = "${azurerm_resource_group.tf_aks_state.name}"
+  resource_group_name   = "${locals.ressource_group_name}"
   storage_account_name  = "${azurerm_storage_account.tf_aks_state_sa.name}"
   container_access_type = "private"
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = "${var.prevent_destroy}"
   }
-
-  #  tags = "${(var.resource_tags, map("Name", "${var.projectname}-${var.location}-tfstate"))}"
 }
--- bootstrap-azure-remote-state/variables.tf
+++ bootstrap-azure-remote-state/variables.tf
@@ -8,10 +8,15 @@ variable "location" {
   type        = "string"
 }
 
+variable "use_ressource_group" {
+  description = "Defines the ressurce group to use. If not set explicitly, a new one will be created"
+  default     = "-"
+}
+
 variable "tf_aks_state_sa_replication_type" {
   description = "set replication type for the Storage Account"
   type        = "string"
-  default     = "LRS"
+  default     = "ZRS"
 }
 
 variable "prevent_destroy" {
@@ -24,12 +29,4 @@ variable "tf_aks_state_sa_tier" {
   description = "Set the Storage Tier e.g. to Standard or Premium"
   type        = "string"
   default     = "Standard"
-}
-
-variable "resource_tags" {
-  type = "map"
-
-  default = {
-    Name = "aks_state"
-  }
 }
